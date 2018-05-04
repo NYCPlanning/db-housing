dd6093898069dd5996c6768294b73e77ad562093
Amanda Doyle <Amanda.Doyle.GIS@gmail.com>
2018-05-04 18:23:42 -0400

    calculating the net change in the number of existing and proposed units

--- housing_build/sql/unitsnet.sql
+++ housing_build/sql/unitsnet.sql
@@ -5,9 +5,7 @@ UPDATE housing
 SET u_net =
 	(CASE
 		WHEN dob_type = 'DM' AND u_init ~ '[0-9]' THEN u_init::numeric * -1
-		WHEN dob_type = 'NB' AND u_prop ~ '[0-9]' THEN u_prop::numeric
-		--WHEN dob_type = 'A1' AND u_init::integer IS NOT NULL AND u_prop::integer IS NOT NULL AND u_prop ~ '[0-9]' AND u_init ~ '[0-9]' THEN u_prop::integer - u_init::integer
+		WHEN dob_type = 'NB' AND u_prop ~ '^[0-9\.]+$' THEN u_prop::numeric
+		WHEN dob_type = 'A1' AND u_init::integer IS NOT NULL AND u_prop::integer IS NOT NULL AND u_prop ~ '^[0-9\.]+$' AND u_init ~ '[0-9]' THEN u_prop::integer - u_init::integer
 		ELSE NULL 
 	END);
-
-	
