c82faf63e287b477a5256811204da35163afe3b6
Amanda Doyle <Amanda.Doyle.GIS@gmail.com>
2018-05-07 13:32:57 -0400

    adding calculation for Complete (demolition)
    value

--- housing_build/sql/dcpstatus.sql
+++ housing_build/sql/dcpstatus.sql
@@ -4,6 +4,14 @@ SET dcp_status = b.dcpstatus
 FROM housing_input_lookup_status b
 WHERE a.status_latest=b.dobstatus;
 
+-- update dcp_status to 'Complete (demolition)' where dob_type ='DM' AND dcp_status = 'Complete' or 'Permit issued'
+UPDATE housing a
+SET dcp_status =
+	(CASE
+		WHEN dob_type = 'DM' AND dcp_status IN ('Complete','Permit issued') THEN 'Complete (demolition)'
+		ELSE dcp_status
+	END);
+
 -- where values were not populated set to be the dob status
 UPDATE housing a
 SET a.dcp_status = INITCAP(a.status_latest)
