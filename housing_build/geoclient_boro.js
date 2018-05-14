95120ef8459eabfa68ce5d26dd2c26419b135519
Amanda Doyle <Amanda.Doyle.GIS@gmail.com>
2018-05-14 14:54:15 -0400

    adding geocoding

--- housing_build/geoclient_boro.js
+++ housing_build/geoclient_boro.js
@@ -18,7 +18,7 @@ var pgp = require('pg-promise')(),
 
 // REQUIRE JS FILE WITH API CREDENTIALS -- USED IN addressLookup FUNCTION
 // ALSO REQUIRES JS SILE WITH DATABASE CONFIGURATION
-var config = require('../dbconfig.js');
+var config = require('../housing.config.js');
 var apiCredentials = require('../apiCredentials.js');
 
 // USE DATABASE CONFIGURATION JS FILE TO LINK TO DATABASE
