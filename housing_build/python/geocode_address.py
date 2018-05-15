795c27bfb4c51895c57aa09ee94deffe796abc40
Amanda Doyle <Amanda.Doyle.GIS@gmail.com>
2018-05-15 12:25:08 -0400

    adding geocoding and geometries

--- housing_build/python/geocode_address.py
+++ housing_build/python/geocode_address.py
@@ -20,15 +20,16 @@ DBUSER = config['DBUSER']
 engine = sql.create_engine('postgresql://{}@localhost:5432/{}'.format(DBUSER, DBNAME))
 
 # load necessary environment variables
+# set variables with following command: export SECRET_KEY="somesecretvalue"
 app_id = os.environ['GEOCLIENT_APP_ID']
 app_key = os.environ['GEOCLIENT_APP_KEY']
 
-# read in cbbr table
-cbbr = pd.read_sql_query('SELECT * FROM housing WHERE address_house IS NOT NULL AND address_street IS NOT NULL AND boro IS NOT NULL AND geom IS NULL;', engine)
+# read in housing table
+housing = pd.read_sql_query('SELECT * FROM housing WHERE address_house IS NOT NULL AND address_street IS NOT NULL AND boro IS NOT NULL AND geom IS NULL;', engine)
 
 # replace single quotes with doubled single quotes for psql compatibility 
-cbbr['address_house'] = [i.replace("'", "''") for i in cbbr['address_house']]
-cbbr['address_street'] = [i.replace("'", "''") for i in cbbr['address_street']]
+housing['address_house'] = [i.replace("'", "''") for i in housing['address_house']]
+housing['address_street'] = [i.replace("'", "''") for i in housing['address_street']]
 
 
 # get the geo data
@@ -75,21 +76,21 @@ def get_loc(num, street, borough):
     return(loc)
 
 locs = pd.DataFrame()
-for i in range(len(cbbr)):
-    new = get_loc(cbbr['address_house'][i],
-                  cbbr['address_street'][i],
-                  cbbr['borough'][i]
+for i in range(len(housing)):
+    new = get_loc(housing['address_house'][i],
+                  housing['address_street'][i],
+                  housing['borough'][i]
     )
     locs = pd.concat((locs, new))
 locs.reset_index(inplace = True)
 
-# update cbbr geom based on bin or lat and long
-for i in range(len(cbbr)):
+# update housing geom based on bin or lat and long
+for i in range(len(housing)):
     if locs['bin'][i] != 'none': 
-        upd = "UPDATE housing a SET geom = ST_Centroid(b.geom), geomsource = 'geoclient', dataname='doitt_buildingfootprints', datasource='doitt' FROM doitt_buildingfootprints b WHERE a.addressnum = '"+ cbbr['addressnum'][i] + "' AND a.streetname = '"+ cbbr['streetname'][i] + "' AND b.bin = '"+ locs['bin'][i] + "';"
+        upd = "UPDATE housing a SET geom = ST_Centroid(b.geom), geomsource = 'geoclient', dataname='doitt_buildingfootprints', datasource='doitt' FROM doitt_buildingfootprints b WHERE a.addressnum = '"+ housing['addressnum'][i] + "' AND a.streetname = '"+ housing['streetname'][i] + "' AND b.bin = '"+ locs['bin'][i] + "';"
         engine.execute(upd)
     elif (locs['lat'][i] != 'none') & (locs['lon'][i] != 'none'):
-        upd = "UPDATE housing a SET geom = ST_SetSRID(ST_MakePoint(" + str(locs['lon'][i]) + ", " + str(locs['lat'][i]) + "), 4326), geomsource = 'geoclient', dataname='lat long', datasource='doitt' WHERE addressnum = '" + cbbr['addressnum'][i] + "' AND a.streetname = '"+ cbbr['streetname'][i] + "';"
+        upd = "UPDATE housing a SET geom = ST_SetSRID(ST_MakePoint(" + str(locs['lon'][i]) + ", " + str(locs['lat'][i]) + "), 4326), geomsource = 'geoclient', dataname='lat long', datasource='doitt' WHERE addressnum = '" + housing['addressnum'][i] + "' AND a.streetname = '"+ housing['streetname'][i] + "';"
         engine.execute(upd)
 
 
