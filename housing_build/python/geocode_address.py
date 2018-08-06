320103ad033e664d2d369c695db3640f0d0905f4
Amanda Doyle <Amanda.Doyle.GIS@gmail.com>
2018-08-06 14:24:52 -0400

    changing location of the app ID

--- housing_build/python/geocode_address.py
+++ housing_build/python/geocode_address.py
@@ -15,14 +15,13 @@ with open('housing.config.json') as conf:
 
 DBNAME = config['DBNAME']
 DBUSER = config['DBUSER']
-
-# connect to postgres db
-engine = sql.create_engine('postgresql://{}@localhost:5432/{}'.format(DBUSER, DBNAME))
-
 # load necessary environment variables
 # set variables with following command: export SECRET_KEY="somesecretvalue"
 app_id = os.environ['GEOCLIENT_APP_ID']
 app_key = os.environ['GEOCLIENT_APP_KEY']
+
+# connect to postgres db
+engine = sql.create_engine('postgresql://{}@localhost:5432/{}'.format(DBUSER, DBNAME))
 
 # read in housing table
 housing = pd.read_sql_query('SELECT DISTINCT address_house, address_street, boro FROM housing WHERE address_house IS NOT NULL AND address_street IS NOT NULL AND address IS NOT NULL AND boro IS NOT NULL AND geom IS NULL;', engine)
