90922807706f1611653ff4c19086eacf6c04aea3
Amanda Doyle <Amanda.Doyle.GIS@gmail.com>
2018-09-26 17:44:49 -0400

    correcting how status q is derived

--- housing_build/sql/statusyear.sql
+++ housing_build/sql/statusyear.sql
@@ -3,5 +3,8 @@ UPDATE housing
 SET status_a_year = RIGHT(status_a, 4)
 WHERE status_a IS NOT NULL;
 UPDATE housing
+SET status_q_year = LEFT(status_q, 4)
+WHERE status_q IS NOT NULL AND status_q LIKE '%-%';
+UPDATE housing
 SET status_q_year = RIGHT(status_q, 4)
-WHERE status_q IS NOT NULL;
+WHERE status_q IS NOT NULL AND status_q LIKE '%/%';
