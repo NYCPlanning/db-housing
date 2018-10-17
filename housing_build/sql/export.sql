a8f036b88a985d945ae25e2f2219dce328fd4bc6
Amanda Doyle <Amanda.Doyle.GIS@gmail.com>
2018-10-17 12:14:30 -0400

    closing issues and adding more customized logic from business owner needs

--- housing_build/sql/export.sql
+++ housing_build/sql/export.sql
@@ -1,10 +1,19 @@
 -- create a tempory table from the housing table where development happened
 -- with the desired fields in the desired order named approriatly
 -- copy that table to the output folder and drop the table
+-- update the cutoff dates
+	-- Cutoff date for 1st quarter would be March 31st.
+	-- Cutoff date for 1st quarter would be June 30th.
+	-- Cutoff date for 3rd quarter is Sept 30th.
+	-- Cutoff date for 4th quarter is Dec 31st.
 DROP TABLE IF EXISTS housing_export;
 CREATE TABLE housing_export AS 
 	(SELECT job_number,job_type,occ_init,occ_prop,dcp_occ_category,dcp_status,dob_status,status_date,status_a,status_a_year,status_d,status_p,status_q,status_q_year,status_r,status_x,stories_init,stories_prop,zoningsft_init,zoningsft_prop,units_init,units_prop,units_net,units_net_complete,units_net_incomplete,earliest_effectivedate,latest_effectivedate,latest_certtype,latest_cofo,unit_change_2007,unit_change_2008,unit_change_2009,unit_change_preapr2010,unit_change_postapr2010,unit_change_2011,unit_change_2012,unit_change_2013,unit_change_2014,unit_change_2015,unit_change_2016,unit_change_2017,unit_change_2018,geo_cd,geo_ntacode,geo_ntaname,geo_censusblock,geo_csd,f_pfirms2015_100yr as pfirms2015_100yr,x_dup_flag,x_dup_id,x_inactive,x_outlier,address,latitude,longitude,ycoord,xcoord,bin,bbl,boro,geom 
-	FROM housing);
+	FROM housing
+	WHERE (earliest_effectivedate::date >= '2010-01-01' AND earliest_effectivedate::date <=  '2018-06-30')
+	OR (earliest_effectivedate IS NULL AND status_q::date >= '2010-01-01' AND status_q::date <=  '2018-06-30')
+	OR (earliest_effectivedate IS NULL AND status_q IS NULL AND status_a::date >= '2010-01-01' AND status_a::date <=  '2018-06-30')
+	);
 -- export
 --all records
 \copy (SELECT * FROM housing_export) TO '/prod/db-housing/housing_build/output/devdb_developments.csv' DELIMITER ',' CSV HEADER;
--- housing_build/sql/qc_outliers.sql
+++ housing_build/sql/qc_outliers.sql
@@ -9,28 +9,44 @@
 -- 5) job_type= A1, TOP 10 for u_prop
 -- 6) job_type= NB and units_init NOT 0
 -- 7) job_description (field does not currently exist) contains both '%RESID%' AND '%HOTEL%'
+-- 8) unit_change_2018 is negative (need to manually update the year)
+
+-- add outlier records to old table
+CREATE TABLE qc_outliersacrhived (
+job_number text, flag text, outlier text);
+INSERT INTO qc_outliersacrhived (
+job_number, flag, outlier)
+SELECT DISTINCT job_number, flag, outlier FROM qc_outliers;
 
 DROP TABLE IF EXISTS qc_outliers;
 CREATE TABLE qc_outliers AS (
-(SELECT * 
+(SELECT *, 
+	'units_net_complete is 50+ units greater than units proposed (NBs only)' as flag,
+	 NULL as outlier
 FROM housing
 WHERE
 	dob_type = 'NB'
 	AND (units_net_complete::integer - units_prop::integer) >= 50)
 UNION
-(SELECT * 
+(SELECT *,
+	'job_type= DM and units_init > 19' as flag,
+	NULL as outlier 
 FROM housing
 WHERE
 	dob_type = 'DM'
 	AND units_init::integer >= 20)
 UNION
-(SELECT * 
+(SELECT *,
+	'job_type= A1 and units_net < -50' as flag,
+	NULL as outlier   
 FROM housing
 WHERE
 	dob_type = 'A1'
 	AND units_net::integer <= -50)
 UNION
-(SELECT * 
+(SELECT *,
+	'top 20 units_net A1' as flag,
+	NULL as outlier 
 FROM housing
 WHERE
 	dob_type = 'A1'
@@ -41,7 +57,9 @@ ORDER BY
 	units_net DESC
 LIMIT 20)
 UNION
-(SELECT * 
+(SELECT *,
+	'top 10 units_prop A1' as flag,
+	NULL as outlier 
 FROM housing
 WHERE
 	dob_type = 'A1'
@@ -52,19 +70,30 @@ ORDER BY
 	units_prop DESC
 LIMIT 10)
 UNION
-(SELECT * 
+(SELECT *,
+	'job_type= NB and units_init NOT 0' as flag,
+	NULL as outlier
 FROM housing
 WHERE
 	dob_type = 'NB'
 	AND units_init::integer <> 0)
 UNION
-(SELECT * 
+(SELECT *,
+	'job_description contains both %RESID% AND %HOTEL%' as flag,
+	NULL as outlier
 FROM housing
 WHERE
 	upper(job_description) LIKE '%RESID%HOTEL%'
 	OR upper(job_description) LIKE '%HOTEL%RESID%')
+UNION
+(SELECT *,
+	'unit_change_2018 is negative' as flag,
+	NULL as outlier    
+FROM housing
+WHERE
+	unit_change_2018::numeric < 0)
 );
 
-\copy (SELECT * FROM qc_outliers) TO '/prod/db-housing/housing_build/output/qc_outliers.csv' DELIMITER ',' CSV HEADER;
+\copy (SELECT * FROM qc_outliers WHERE job_number NOT IN (SELECT DISTINCT job_number FROM qc_outliersacrhived WHERE outlier = 'N')) TO '/prod/db-housing/housing_build/output/qc_outliers.csv' DELIMITER ',' CSV HEADER;
 
 DROP TABLE IF EXISTS qc_outliers;
