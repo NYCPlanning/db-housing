8dc9264bdb8a3df7518df2676b6cd0af113fdfef
Amanda Doyle <Amanda.Doyle.GIS@gmail.com>
2018-10-17 15:20:11 -0400

    changing the_geom to geom

--- housing_build/sql/coods.sql
+++ housing_build/sql/coods.sql
@@ -1,8 +1,8 @@
 -- Set the Lat/Long and X/Y 
 UPDATE housing
 SET
-	latitude = ST_Y(the_geom),
-	longitude = ST_X(the_geom),
-	ycoord = ST_Y(ST_Transform(the_geom, 2263)),
-	xcoord = ST_X(ST_Transform(the_geom, 2263))
+	latitude = ST_Y(geom),
+	longitude = ST_X(geom),
+	ycoord = ST_Y(ST_Transform(geom, 2263)),
+	xcoord = ST_X(ST_Transform(geom, 2263))
 	WHERE geom IS NOT NULL;
