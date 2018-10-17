d8cddc22131b98af93ff61be00ce2007520319f6
Amanda Doyle <Amanda.Doyle.GIS@gmail.com>
2018-10-17 16:29:46 -0400

    adding logic to create geometry from lat / long if lat is not null and geom is null

--- housing_build/sql/coods.sql
+++ housing_build/sql/coods.sql
@@ -6,3 +6,7 @@ SET
 	ycoord = ST_Y(ST_Transform(geom, 2263)),
 	xcoord = ST_X(ST_Transform(geom, 2263))
 	WHERE geom IS NOT NULL;
+-- If there is no geometry but there is a lat / long then create the geometry
+UPDATE housing
+SET geom = ST_SetSRID(ST_MakePoint(longitude::double precision, latitude::double precision), 4326)
+WHERE geom IS NULL AND longitude IS NOT NULL;
