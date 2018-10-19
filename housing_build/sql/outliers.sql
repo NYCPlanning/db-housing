1c807117ca609986c4fc3ad83ae6487f69ac70fe
Amanda Doyle <Amanda.Doyle.GIS@gmail.com>
2018-10-19 10:50:19 -0400

    removing second where

--- housing_build/sql/outliers.sql
+++ housing_build/sql/outliers.sql
@@ -116,7 +116,7 @@ SET x_outlier = TRUE
 WHERE job_number IN
 	(SELECT DISTINCT job_number
 		FROM qc_outliers
-		WHERE outlier <> 'N' OR  WHERE outlier <> 'C');
+		WHERE outlier <> 'N' OR outlier <> 'C');
 
 -- Remove the data table
 DROP TABLE IF EXISTS qc_outliers;
